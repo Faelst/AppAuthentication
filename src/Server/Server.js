11fb68c912eaeca37221d1359b32fecb484f69fd
fael_st <rafael.silverio@vivasinternet.com.br>
2020-07-27 10:15:32 -0300

    Auterações para fase de teste da aplicação

--- src/Server/Server.js
+++ src/Server/Server.js
@@ -2,10 +2,11 @@
 const express = require('express')
 const server = express()
 const router = express.Router();
-const port =  process.env.PORT || 3030;
+const port =  process.env.PORT || 8080;
+const cors = require('..\\Config\\Cors.js')
 
-// server.use(cors)
-server.listen(port ,() => console.log(`http://10.16.128.109:${port}`))
+server.use(cors)
+server.listen(port ,() => console.log(`http://177.126.240.61:${port}`))
 server.use('/api' , router)
 
 module.exports = router
